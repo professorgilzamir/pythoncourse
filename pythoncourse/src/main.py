f72e0b533888cc7689b4e57d127994b05bddca8c
user <user@labcomp48726u>
2017-03-16 15:17:51 -0300

    haha

--- pythoncourse/src/main.py
+++ pythoncourse/src/main.py
@@ -15,8 +15,8 @@ import os
 
 def main():
     RESOURCE_URL = "http://repositorio.dados.gov.br/saude/unidades-saude/unidade-basica-saude/ubs.csv.zip"
-    OUTPUT_PATH = "/home/gil/saida.zip"
-    EXTRACTED_PATH = "/home/gil/" 
+    OUTPUT_PATH = "/home/user/gil/saida.zip"
+    EXTRACTED_PATH = "/home/user/gil/" 
     if len(sys.argv) > 1:
        RESOURCE_URL = sys.argv[1] 
     if len(sys.argv) > 2:
