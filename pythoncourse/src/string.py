b2d630e5aaa198ee9b3405191017c07cf753c11c
odeveloper <odeveloper@compScientist.debian>
2017-03-22 23:52:28 -0300

    Metodo extract_filename melhorado

---
+++ pythoncourse/src/string.py
@@ -0,0 +1,13 @@
+
+def add (x, y): return x + y
+
+def extract_filename(filename):
+    arr = filename.split('.')
+    arr.pop()
+    str = ""
+    for x in arr:
+    	str = str + x + "."
+
+    return str[:len(str)-1]
+
+print (extract_filename("this.is.mega"))
