e9a2ee09e4e2b99ba0c8928e3e77113c96a84a82
gilza <gilza@DESKTOP-89F3IPR>
2017-04-20 07:14:34 -0300

    index created.

--- pythoncourse/src/main.py
+++ pythoncourse/src/main.py
@@ -15,33 +15,19 @@ import os
 
 def main():
     RESOURCE_URL = "http://repositorio.dados.gov.br/saude/unidades-saude/unidade-basica-saude/ubs.csv.zip"
+    OUTPUT_PATH = "./dt.zip";
+    EXTRACTION_PATH = "./"
+    
     if len(sys.argv) > 1:
         RESOURCE_URL = sys.argv[1] 
     if len(sys.argv) > 2:
         OUTPUT_PATH = sys.argv[2]
     if len(sys.argv) > 3:
-        EXTRACTED_PATH = sys.argv[3]
-    response = request.urlopen(RESOURCE_URL)
-    out_file = io.FileIO(OUTPUT_PATH, mode="w")
-    content_length = response.getheader('Content-Length')
-    if content_length:
-        length = int(content_length)
-        dw.download_length(response, out_file, length)
-    else:
-        dw.download(response, out_file)
-    zfile = zipfile.ZipFile(OUTPUT_PATH)
-    zfile.extractall(EXTRACTED_PATH)
+        EXTRACTION_PATH = sys.argv[3]
     
-    filename = [name for name in os.listdir(EXTRACTED_PATH) if '.csv' in name]
-    
-    
-    dt = dw.read_data(EXTRACTED_PATH+filename[0])
-    
-    for t in dt:
-        print(t) 
-    
-    response.close()
-    out_file.close()
+    dt = dw.loadlistfromcsv(RESOURCE_URL, OUTPUT_PATH, EXTRACTION_PATH)
+    index = dw.create_cidcnes_index(dt)
+    print(index["3305402294877"]);
     print("Finished")
 
 if __name__ == "__main__":
