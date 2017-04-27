b56c2ac1ec8d87937572ce93106d7b35517658e5
ElissonMatos <matos.elisson@gmail.com>
2017-04-27 09:37:27 -0300

    Create NumeroTelefoneInvalido.py

---
+++ pythoncourse/src/NumeroTelefoneInvalido.py
@@ -0,0 +1,19 @@
+class NumeroTelefoneInvalido(Exception):
+
+  def __init__(self, numero, mensagem = "Numerx'o de telefone invalido"):
+    self._numero = numero
+    self._mensagem = mensagem
+
+  def magicGet(self, atrib):
+    if atrib == 'numero':
+      return self._numero
+    elif atrib == 'mensagem':
+      return self._mensagem
+    else:
+      return None
+
+  def magicSet(self, atrib, value):
+    if atrib == 'numero'
+      self._numero = value
+    elif atrib == 'mensagem'
+      self._mensagem = value
