c3be86a968bbecb7f16cd3a28fb14bf7ac15bbe3
Luis Costa <lu15c05ta@hotmail.com>
2017-04-27 14:37:01 -0300

    Update classes.py

--- pythoncourse/src/classes.py
+++ pythoncourse/src/classes.py
@@ -3,19 +3,11 @@ class LocalizacaoGeografica:
 		self._latidude = latitude
 		self._longitude = longitude
 
-	def magicGet(self, atrib):
-		if atrib == 'latitude':
-			return self._latidude
-		elif atrib == 'longitude':
-			return self._longitude
-		else:
-			return None
-
-	def magicSet(self, atrib, value):
-		if atrib == 'latitude':
-			self._latitude = value
-		elif atrib == 'longitude':
-			self._longitude = value
+	def mget(self, prop):
+		return self.__dict__[prop]
+	
+	def mset(self, prop, value):
+		self.__dict__[prop] = value
 
 class UnidadeDeSaude(LocalizacaoGeografica):
 	def __init__(self, latitude, longitude, codCid, codCnes, dscEstFisAmb, dscAdapFisldo, sitEquipamentos, endereco):
@@ -27,43 +19,11 @@ class UnidadeDeSaude(LocalizacaoGeografica):
 		self._codCid = codCid
 		self._codCnes = codCnes
 
-	def magicGet(self, atrib):
-		if atrib == 'latitude':
-			return self._latitude
-		elif atrib == 'longitude':
-			return self._longitude
-		elif atrib == 'codCid':
-			return self._codCid
-		elif atrib == 'codCnes':
-			return self._codCnes
-		elif atrib == 'dscEstFisAmb':
-			return self._dscEstFisAmb
-		elif atrib == 'dscAdapFisldo':
-			return self._dscAdapFisldo
-		elif atrib == 'sitEquipamentos':
-			return self._sitEquipamentos
-		elif atrib == 'endereco':
-			return self._endereco
-		else:
-			return None
-
-	def magicSet(self, atrib, value):
-		if atrib == 'latitude':
-			self._latitude = value
-		elif atrib == 'longitude':
-			self._longitude = value
-		elif atrib == 'codCid':
-			self._codCid = value
-		elif atrib == 'codCnes':
-			self._codCnes = value
-		elif atrib == 'dscEstFisAmb':
-			self._dscEstFisAmb = value
-		elif atrib == 'dscAdapFisldo':
-			self._dscAdapFisldo = value
-		elif atrib == 'sitEquipamentos':
-			self._sitEquipamentos = value
-		elif atrib == 'endereco':
-			self._endereco = value
+	def mget(self, prop):
+		return self.__dict__[prop]
+	
+	def mset(self, prop, value):
+		self.__dict__[prop] = value
 
 
 class UnidadeDeSaude(LocalizacaoGeografica):
@@ -76,59 +36,19 @@ class UnidadeDeSaude(LocalizacaoGeografica):
 		self._sitEquipamentos = sitEquipamentos
 		self._endereco = endereco
 
-	def magicGet(self, atrib):
-		if atrib == 'latitude':
-			return self._latitude
-		elif atrib == 'longitude':
-			return self._longitude
-		elif atrib == 'codCid':
-			return self._codCid
-		elif atrib == 'codCnes':
-			return self._codCnes
-		elif atrib == 'dscEstFisAmb':
-			return self._dscEstFisAmb
-		elif atrib == 'dscAdapFisldo':
-			return self._dscAdapFisldo
-		elif atrib == 'sitEquipamentos':
-			return self._sitEquipamentos
-		elif atrib == 'endereco':
-			return self._endereco
-		else:
-			return None
-
-	def magicSet(self, atrib, value):
-		if atrib == 'latitude':
-			self._latitude = value
-		elif atrib == 'longitude':
-			self._longitude = value
-		elif atrib == 'codCid':
-			self._codCid = value
-		elif atrib == 'codCnes':
-			self._codCnes = value
-		elif atrib == 'dscEstFisAmb':
-			self._dscEstFisAmb = value
-		elif atrib == 'dscAdapFisldo':
-			self._dscAdapFisldo = value
-		elif atrib == 'sitEquipamentos':
-			self._sitEquipamentos = value
-		elif atrib == 'endereco':
-			self._endereco = value
-
+	def mget(self, prop):
+		return self.__dict__[prop]
+	
+	def mset(self, prop, value):
+		self.__dict__[prop] = value
+	
 class NumeroTelefoneInvalido(Exception):
 	def __init__(self, numero, mensagem = "Numero de telefone invalido"):
 		self._numero = numero
 		self._mensagem = mensagem
 
-	def magicGet(self, atrib):
-		if atrib == 'numero':
-			return self._numero	
-		elif atrib == 'mensagem':
-			return self._mensagem
-		else:
-			return None
-
-	def magicSet(self, atrib, value):
-		if atrib == 'numero'
-			self._numero = value
-		elif atrib == 'mensagem'
-			self._mensagem = value
+	def mget(self, prop):
+		return self.__dict__[prop]
+	
+	def mset(self, prop, value):
+		self.__dict__[prop] = value
