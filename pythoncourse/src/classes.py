dbc8512ea43490f3b196f961b9487c48d9bc5fe3
Luis Costa <lu15c05ta@hotmail.com>
2017-04-27 16:22:21 -0300

    Update classes.py

--- pythoncourse/src/classes.py
+++ pythoncourse/src/classes.py
@@ -8,23 +8,30 @@ class LocalizacaoGeografica:
 	
 	def mset(self, prop, value):
 		self.__dict__[prop] = value
-
-class UnidadeDeSaude(LocalizacaoGeografica):
-	def __init__(self, latitude, longitude, codCid, codCnes, dscEstFisAmb, dscAdapFisldo, sitEquipamentos, endereco):
-		super().__init__(latitude, longitude)
-		self._dscEstFisAmb = dscEstFisAmb
-		self._dscAdapFisldo = dscAdapFisldo
-		self._sitEquipamentos = sitEquipamentos
-		self._endereco = endereco
-		self._codCid = codCid
-		self._codCnes = codCnes
-
+		
+		
+		
+		
+class Endereco:
+	def __init__(self, logradouro, bairro, cidade, telefone):
+		self._logradouro = logradouro
+		self._bairro = bairro
+		self._cidade = cidade
+		self._telefone = telefone
+	
 	def mget(self, prop):
 		return self.__dict__[prop]
 	
 	def mset(self, prop, value):
 		self.__dict__[prop] = value
-
+	
+	def validarTelefone(telefone):
+		t = telefone
+		if ((len(t) == 13 or len(t) == 14) and (t[0] == '(' and t[3] == ')')
+			print "Telefone Validado"
+		else
+			raise NumeroDeTelefoneInvalido(1)	
+		
 
 class UnidadeDeSaude(LocalizacaoGeografica):
 	def __init__(self, latitude, longitude, codCid, codCnes, dscEstFisAmb, dscAdapFisldo, sitEquipamentos, endereco):
